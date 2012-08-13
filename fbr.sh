e32d0090352341d13f16b2ccf8d468b54110fff9
Jon Kagan <darkspadez@gmail.com>
2012-08-12 21:17:38 -0400

    v0.1.1

--- fbr.sh
+++ fbr.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Finna Be Robot
-# Version 0.1
+# Version 0.1.1
 # Written by Jonathan "darkspadez" Kagan - http://darkspadez.com
 # Last modified on 12 August, 2012
 
 # First let's find out what they wanna do
-echo For the following either enter \"yes\" or \"no\"
+echo For the following either enter \"y\", for yes, or \"n\", for no.
 
 # Repo Sync
 echo Do you wanna run a repo sync first\?
@@ -13,14 +13,14 @@ read runSync
 
 # -j#
 echo Set -j\#
-echo Number only. Will be used if you sync and/or build.
+echo Number only. Will be used \if you sync and/or build.
 read runJ
 
 # Build
 echo Do you wish to build?
 read runBuild
 
-if [ $runBuild = "yes" ]; then
+if [ $runBuild = "y" ]; then
 
     # Clobber
     echo Do you wanna clobber first\?
@@ -28,7 +28,7 @@ if [ $runBuild = "yes" ]; then
 
     # Out
     echo Is your build directory \"out\"\?
-    echo If it is type \"yes\", if not type the directory out like \"out\".
+    echo If it is type \"yes\", \if not type the directory out like \"out\".
     read setClobber
 
     # Time
@@ -43,31 +43,32 @@ if [ $runBuild = "yes" ]; then
 fi
 
 # Check answer for sync
-if [ $runSync = "yes" ]; then
+if [ $runSync = "y" ]; then
     repo sync -j$runJ
 fi
 
 # Check answer for clobber
-if [ $runClobber = "yes" ]; then
-    if [ $setClobber = "yes" ]; then
+if [ $runClobber = "y" ]; then
+    if [ $setClobber = "y" ]; then
         rm -rf out
-	else
-	    rm -rf $setClobber
-	fi
+    else
+        rm -rf $setClobber
+    fi
 fi
 
 # Build
-if [ $runBuild = "yes" ]; then
+if [ $runBuild = "y" ]; then
 
     # Run envsetup.sh
     . build/envsetup.sh
 	
-	# Pick Device
-	lunch $runDevice
-	
-	if [ $runTime = "yes" ]; then
-	    time make -j$runJ bacon
-	else
-	    make -j$runJ bacon
-	fi
+    # Pick Device
+    lunch $runDevice
+
+    # Run the build
+    if [ $runTime = "y" ]; then
+        time make -j$runJ bacon
+    else
+        make -j$runJ bacon
+    fi
 fi
